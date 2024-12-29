601dedb0c7438be891c01ff6fa7e5191c4500c60
davelpz <davelpz@gmail.com>
2024-12-29 11:18:20 -0500

    adding logo

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,5 +1,6 @@
 import { Scene } from 'phaser';
 
+// MainMenu Scene - This is the main menu for the game
 export class MainMenu extends Scene
 {
     constructor ()
@@ -14,13 +15,15 @@ export class MainMenu extends Scene
 
         this.add.image(width/2, height/2, 'background');
 
-        this.add.image(width/2, height/2, 'logo');
+        let logo = this.add.image(width/2, height/2, 'logo');
+        // Shrink the image by 1/4
+        logo.setScale(0.25);
 
-        this.add.text(width/2, height/2 + 100, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        // this.add.text(width/2, height/2 + 300, 'Start', {
+        //     fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        //     stroke: '#000000', strokeThickness: 8,
+        //     align: 'center'
+        // }).setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
 
